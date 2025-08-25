6bd4213cc0197ecdf17416aec6b421336bd6c565
Hong Zhang Lim <54992455+HongZhangLim@users.noreply.github.com>
2025-08-25 09:03:14 +0000

    Add Week 1 Home Federal Savings Bank problem.

---
+++ Week1/bank.py
@@ -0,0 +1,10 @@
+# Assign variable greet, accept input
+greet = input("Greeting: ")
+greet = greet.strip().lower()
+
+if greet.startswith("hello"):
+    print("$0")
+elif greet.startswith("h"):
+    print("$20")
+else:
+    print("$100")
