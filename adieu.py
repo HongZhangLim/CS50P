5e978c99db325d70313d05be332671efe41d153c
Hong Zhang Lim <54992455+HongZhangLim@users.noreply.github.com>
2025-09-23 21:40:04 +0000

    Add Week 4 Adieu, Adieu problem.

---
+++ adieu.py
@@ -0,0 +1,15 @@
+import inflect
+p = inflect.engine()
+
+name_list = []
+
+while True:
+    try:
+        name = input("Name: ")
+        name_list.append(name)
+    except EOFError:
+        print("\n")
+        break
+
+print("Adieu, adieu, to " + p.join(name_list))
+    
