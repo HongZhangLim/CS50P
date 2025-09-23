609f3ddbd57ef86438f50c26ab180676d72b5c1b
Hong Zhang Lim <54992455+HongZhangLim@users.noreply.github.com>
2025-09-23 21:41:17 +0000

    Add Week 4 Guessing Game problem.

---
+++ game.py
@@ -0,0 +1,24 @@
+import random
+
+while True:
+    n = int(input("Level: "))
+    if not n <= 0:
+        break
+ans = random.randint(1, n)
+
+while True:
+    try:
+        guess = int(input("Guess: "))
+    except ValueError:
+        continue
+    #nonpositive include 0
+    if guess < 1:
+        continue
+    if guess == ans:
+        print("Just right!")
+        break
+    elif guess < ans:
+        print("Too Small!")
+    else:
+        print("Too large!")
+
